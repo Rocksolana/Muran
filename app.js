83c6511afb48bafcc0c46941b723a3aa84b11b17
ВАШЕ_ИМЯ <tsypuk@gmail.com>
2021-03-01 21:50:05 +0200

    added navigtion menu

--- app.js
+++ app.js
@@ -14,3 +14,13 @@ if (mobileMenu.classList.contains("active-menu")){
 $(".slider-block").slick({
     dots:true
 });
+
+$(document).ready(function(){
+    $(".seit_navigation").on("click","a", function (event) {
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 1500);
+    });
+  });
